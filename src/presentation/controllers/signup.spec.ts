3659be1435ca4bc74a0a919f29c811aa7a6a5539
moniz Massango <moniz.massango@gmail.com>
2022-08-04 20:22:06 +0200

    test: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -42,3 +42,16 @@ test('should return 400 if no password is provided', () => {
   const httpResponse = sut.handle(httpRequest)
   expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
 })
+
+test('should return 400 if no passwordConfirmation is provided', () => {
+  const sut = new SignUpController()
+  const httpRequest = {
+    body: {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+  }
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirmation')))
+})
