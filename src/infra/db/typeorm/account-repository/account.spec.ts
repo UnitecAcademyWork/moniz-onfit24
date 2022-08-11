7a583062d0390f4d9288bd2595888e916746fb2d
moniz Massango <moniz.massango@gmail.com>
2022-08-11 21:31:13 +0200

    test: ensure AccountRepository returns an account on success

---
+++ src/infra/db/typeorm/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { AccountRepository } from './account'
+import { TypeormHelper } from '../typeorm-helper'
+
+describe('Account Repository', () => {
+  beforeAll(async () => {
+    await TypeormHelper.create()
+  })
+  afterAll(async () => {
+    await TypeormHelper.clear()
+    await TypeormHelper.close()
+  })
+
+  test('should return an account on success', async () => {
+    const sut = new AccountRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
