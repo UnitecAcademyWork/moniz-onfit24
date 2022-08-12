c5c409c8c03904e3dd958461cd29eb7246a0e756
moniz Massango <moniz.massango@gmail.com>
2022-08-12 15:59:56 +0200

    refactor: connect to databaase before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
 import app from '../config/app'
+import { TypeormHelper } from '../../infra/db/typeorm/typeorm-helper'
 
 describe('SignUp Route', () => {
+  beforeAll(async () => {
+    await TypeormHelper.create()
+  })
+  afterAll(async () => {
+    await TypeormHelper.close()
+  })
+  beforeEach(async () => {
+    await TypeormHelper.clear()
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
