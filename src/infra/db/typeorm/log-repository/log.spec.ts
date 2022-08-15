36542da6412f91c834a1873574fa08314d4b6231
moniz Massango <moniz.massango@gmail.com>
2022-08-15 11:20:16 +0200

    refactor: move sut creation to factory method

--- src/infra/db/typeorm/log-repository/log.spec.ts
+++ src/infra/db/typeorm/log-repository/log.spec.ts
@@ -2,6 +2,10 @@ import { LogError } from '../entities/log-error'
 import { TypeormHelper } from '../typeorm-helper'
 import { LogRepository } from './log'
 
+const makeSut = (): LogRepository => {
+  return new LogRepository()
+}
+
 describe('Log Repository', () => {
   beforeAll(async () => {
     await TypeormHelper.create()
@@ -14,7 +18,7 @@ describe('Log Repository', () => {
   })
 
   test('should create an error log on success', async () => {
-    const sut = new LogRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await LogError.count()
     expect(count).toBe(1)
