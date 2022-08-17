279f0e266d32e29abdd5a4218695ee098f9531ba
moniz Massango <moniz.massango@gmail.com>
2022-08-17 11:58:09 +0200

    feat: ensure AccountRepository returns an account on loadByEmail success

--- src/infra/db/typeorm/account-repository/account.ts
+++ src/infra/db/typeorm/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { Account } from '../entities/account'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const account = new Account()
     account.name = accountData.name
@@ -11,5 +12,10 @@ export class AccountRepository implements AddAccountRepository {
     account.password = accountData.password
     const result = await account.save()
     return result
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const account = await Account.findOneBy({ email })
+    return account
   }
 }
