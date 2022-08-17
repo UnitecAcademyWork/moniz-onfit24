edd978f62af2514e986422a39947d699df8f7165
moniz Massango <moniz.massango@gmail.com>
2022-08-17 11:24:28 +0200

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -24,4 +24,11 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_value')
     expect(accessToken).toBe('any_token')
   })
+
+  test('should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_values')
+    await expect(promise).rejects.toThrow()
+  })
 })
