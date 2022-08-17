06c15b619375250f6bc1d38d1450deb3eb265105
moniz Massango <moniz.massango@gmail.com>
2022-08-17 11:58:32 +0200

    test: ensure AccountRepository returns an account on loadByEmail success

--- src/infra/db/typeorm/account-repository/account.spec.ts
+++ src/infra/db/typeorm/account-repository/account.spec.ts
@@ -1,5 +1,6 @@
 import { AccountRepository } from './account'
 import { TypeormHelper } from '../typeorm-helper'
+import { AccountModel } from '../../../../domain/models/account'
 
 describe('Account Repository', () => {
   beforeAll(async () => {
@@ -12,17 +13,32 @@ describe('Account Repository', () => {
     await TypeormHelper.clear()
   })
 
-  const makeSut = (): AccountRepository => {
-    return new AccountRepository()
-  }
-
-  test('should return an account on success', async () => {
+  const makeAddAccount = async (): Promise<AccountModel> => {
     const sut = makeSut()
-    const account = await sut.add({
+    return await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+  }
+
+  const makeSut = (): AccountRepository => {
+    return new AccountRepository()
+  }
+
+  test('should return an account on add success', async () => {
+    const account = await makeAddAccount()
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await makeAddAccount()
+    const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
