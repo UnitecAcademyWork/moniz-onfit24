e60bb361491953b7f34fe68f4144d1d9e910f1a6
moniz Massango <moniz.massango@gmail.com>
2022-08-18 11:57:25 +0200

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest'
+import { hash } from 'bcrypt'
 import app from '../config/app'
 import { TypeormHelper } from '../../infra/db/typeorm/typeorm-helper'
+import { Account } from '../../infra/db/typeorm/entities/account'
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -22,6 +24,24 @@ describe('Login Routes', () => {
           email: 'lambo@gmail.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123456', 12)
+      const account = new Account()
+      account.name = 'lambo'
+      account.email = 'lambo@gmail.com'
+      account.password = password
+      await account.save()
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lambo@gmail.com',
+          password: '123456'
         })
         .expect(200)
     })
