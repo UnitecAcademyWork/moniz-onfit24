50c7a493baf9f36b8aab49eb725fcb1d2bafab31
moniz Massango <moniz.massango@gmail.com>
2022-08-18 12:10:33 +0200

    feat: ensure ExpressAdapter returns the error message instead of the error name

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,6 +3,7 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adpter'
 import { AccountRepository } from '../../infra/db/typeorm/account-repository/account'
+import { LogRepository } from '../../infra/db/typeorm/log-repository/log'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
 
@@ -13,5 +14,6 @@ export const makeSignUpController = (): Controller => {
   const accountRepository = new AccountRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logRepository = new LogRepository()
+  return new LogControllerDecorator(signUpController, logRepository)
 }
