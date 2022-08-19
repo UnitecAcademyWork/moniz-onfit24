d0b594e4a320da68c294ad3acb29daa2757206f9
moniz Massango <moniz.massango@gmail.com>
2022-08-19 15:43:45 +0200

    test: ensure ObjectiveController returns 400 if Validation returns an error

--- src/presentation/controllers/objective/objective-controller.spec.ts
+++ src/presentation/controllers/objective/objective-controller.spec.ts
@@ -1,5 +1,7 @@
 import { ObjectiveController } from './objective-controller'
 import { HttpRequest, Validation } from './objective-controller.protocols'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { MissingParamError } from '../../errors'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -36,5 +38,12 @@ describe('Objective Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
