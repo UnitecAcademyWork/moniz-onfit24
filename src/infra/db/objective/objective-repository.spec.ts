cf3b38b13df50ff90c62f6e9fca040093aa05da1
moniz Massango <moniz.massango@gmail.com>
2022-08-20 09:45:42 +0200

    test: ensure ObjectiveRepository returns an objective on add success

---
+++ src/infra/db/objective/objective-repository.spec.ts
@@ -0,0 +1,28 @@
+import { TypeormHelper } from '../typeorm-helper'
+import { ObjectiveRepository } from './objective-repository'
+
+const makeSut = (): ObjectiveRepository => {
+  return new ObjectiveRepository()
+}
+
+describe('Objective Repository', () => {
+  beforeAll(async () => {
+    await TypeormHelper.create()
+  })
+  afterAll(async () => {
+    await TypeormHelper.close()
+  })
+  beforeEach(async () => {
+    await TypeormHelper.clear()
+  })
+
+  test('should return an objective on add success', async () => {
+    const sut = makeSut()
+    const objective = await sut.add({ name: 'valid_name', icon: 'valid_icon', description: 'valid_description' })
+    expect(objective).toBeTruthy()
+    expect(objective.id).toBeTruthy()
+    expect(objective.name).toBe('valid_name')
+    expect(objective.icon).toBe('valid_icon')
+    expect(objective.description).toBe('valid_description')
+  })
+})
