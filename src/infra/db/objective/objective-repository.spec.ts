627308e9f6212c35b8a047989c218265f4f636a5
moniz Massango <moniz.massango@gmail.com>
2022-08-20 09:54:54 +0200

    test: ensure ObjectiveRepository returns an objective on loadByName success

--- src/infra/db/objective/objective-repository.spec.ts
+++ src/infra/db/objective/objective-repository.spec.ts
@@ -25,4 +25,15 @@ describe('Objective Repository', () => {
     expect(objective.icon).toBe('valid_icon')
     expect(objective.description).toBe('valid_description')
   })
+
+  test('should return an objective on loadByName success', async () => {
+    const sut = makeSut()
+    await sut.add({ name: 'valid_name', icon: 'valid_icon', description: 'valid_description' })
+    const objective = await sut.loadByName('valid_name')
+    expect(objective).toBeTruthy()
+    expect(objective.id).toBeTruthy()
+    expect(objective.name).toBe('valid_name')
+    expect(objective.icon).toBe('valid_icon')
+    expect(objective.description).toBe('valid_description')
+  })
 })
