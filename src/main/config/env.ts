ad3cae8ac8ffdbf1ea622a4b7059b3a5127f1c34
moniz Massango <moniz.massango@gmail.com>
2022-08-22 00:24:13 +0200

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
-  name: process.env.NAME || 'default',
-  type: process.env.TYPE || 'better-sqlite3',
-  database: process.env.DATABASE || ':memory:',
-  port: process.env.PORT || 5000,
-  jwtsecret: process.env.JWT_SECRET || '8JdsZ!@k)Djs'
+  name: process.env.NAME ?? 'default',
+  type: process.env.TYPE ?? 'better-sqlite3',
+  database: process.env.DATABASE ?? ':memory:',
+  port: process.env.PORT ?? 5000,
+  jwtsecret: process.env.JWT_SECRET ?? '8JdsZ!@k)Djs'
 }
