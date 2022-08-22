839a896f67dbd645dd4b0d7fb6ecc52afe96fcf4
moniz Massango <moniz.massango@gmail.com>
2022-08-22 12:56:50 +0200

    test: ensure AccountRepository find account by token with role

--- src/infra/db/account/account-repository.spec.ts
+++ src/infra/db/account/account-repository.spec.ts
@@ -67,4 +67,42 @@ describe('Account Repository', () => {
       expect(result.accessToken).toBe('any_token')
     })
   })
+
+  describe('updateUserRole()', () => {
+    test('should update the account role on updateUserRole success', async () => {
+      const account = await makeAddAccount()
+      const sut = makeSut()
+      await sut.updateRole(account.id, 'any_role')
+      const result = await Account.findOneBy({ id: account.id })
+      expect(result).toBeTruthy()
+      expect(result.role).toBe('any_role')
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      const newAccount = await makeAddAccount()
+      await sut.updateAccessToken(newAccount.id, 'any_token')
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      const newAccount = await makeAddAccount()
+      await sut.updateAccessToken(newAccount.id, 'any_token')
+      await sut.updateRole(newAccount.id, 'any_role')
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
 })
