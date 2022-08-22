cadca07339942c403b08edb73bacff439a8d2950
moniz Massango <moniz.massango@gmail.com>
2022-08-22 11:51:41 +0200

    refactor: separete tests in describe blocks

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -12,23 +12,25 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  test('should call sign with correct values', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt('any_value')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
-  })
+  describe('Sign()', () => {
+    test('should call sign with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_value')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+    })
 
-  test('should return a token on sign success', async () => {
-    const sut = makeSut()
-    const accessToken = await sut.encrypt('any_value')
-    expect(accessToken).toBe('any_token')
-  })
+    test('should return a token on sign success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_value')
+      expect(accessToken).toBe('any_token')
+    })
 
-  test('should throw if sign throws', async () => {
-    const sut = makeSut()
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
-    const promise = sut.encrypt('any_values')
-    await expect(promise).rejects.toThrow()
+    test('should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_values')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
--- src/infra/db/account/account-repository.spec.ts
+++ src/infra/db/account/account-repository.spec.ts
@@ -27,38 +27,44 @@ describe('Account Repository', () => {
     return new AccountRepository()
   }
 
-  test('should return an account on add success', async () => {
-    const account = await makeAddAccount()
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+  describe('add()', () => {
+    test('should return an account on add success', async () => {
+      const account = await makeAddAccount()
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
   })
 
-  test('should return an account on loadByEmail success', async () => {
-    const sut = makeSut()
-    await makeAddAccount()
-    const account = await sut.loadByEmail('any_email@mail.com')
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+  describe('loadByEmail()', () => {
+    test('should return an account on loadByEmail success', async () => {
+      const sut = makeSut()
+      await makeAddAccount()
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeFalsy()
+    })
   })
 
-  test('should return null if loadByEmail fails', async () => {
-    const sut = makeSut()
-    const account = await sut.loadByEmail('any_email@mail.com')
-    expect(account).toBeFalsy()
-  })
-
-  test('should update the account accessToken on updateAccessToken success', async () => {
-    const account = await makeAddAccount()
-    const sut = makeSut()
-    await sut.updateAccessToken(account.id, 'any_token')
-    const result = await Account.findOneBy({ id: account.id })
-    expect(result).toBeTruthy()
-    expect(result.accessToken).toBe('any_token')
+  describe('updateAccessToken()', () => {
+    test('should update the account accessToken on updateAccessToken success', async () => {
+      const account = await makeAddAccount()
+      const sut = makeSut()
+      await sut.updateAccessToken(account.id, 'any_token')
+      const result = await Account.findOneBy({ id: account.id })
+      expect(result).toBeTruthy()
+      expect(result.accessToken).toBe('any_token')
+    })
   })
 })
