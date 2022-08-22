5f7d87d571329ef626919ba847d020389b7e3eb9
moniz Massango <moniz.massango@gmail.com>
2022-08-22 11:59:30 +0200

    test: ensure JwtAdapter calls verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -12,6 +12,6 @@ export class JwtAdapter implements Encrypter, Decrypter {
 
   async decrypt (value: string): Promise<string> {
     await jwt.verify(value, this.secret)
-    return Promise.resolve(null)
+    return null
   }
 }
