4cae3648745072912ef6d42e89c3b96abb5bcfec
moniz Massango <moniz.massango@gmail.com>
2022-08-23 13:57:38 +0200

    feat: ensure UploadController returns 500 if FileUpload throws

--- src/presentation/controllers/upload/upload-controller.ts
+++ src/presentation/controllers/upload/upload-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, ok } from '@/presentation/helpers/http/http-helper'
+import { badRequest, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, FileUpload, HttpRequest, HttpResponse, Validation } from './upload-controller.protocols'
 
 export class UploadController implements Controller {
@@ -8,12 +8,16 @@ export class UploadController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (!error) {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { name, tempFilePath, mimetype, size } = httpRequest.body.file
       const url = await this.fileUpload.upload({ name, tempFilePath, mimetype, size })
       return ok({ url })
+    } catch (error) {
+      return serverError(error)
     }
-    return badRequest(error)
   }
 }
