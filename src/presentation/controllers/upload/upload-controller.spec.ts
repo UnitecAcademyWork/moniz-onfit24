9faef086f7f83bb4ed09cfd2329695b5e77b3385
moniz Massango <moniz.massango@gmail.com>
2022-08-23 13:46:22 +0200

    feat: ensure UploadController calls Validation with correct values and returns error 400 if validation fails

--- src/presentation/controllers/upload/upload-controller.spec.ts
+++ src/presentation/controllers/upload/upload-controller.spec.ts
@@ -1,5 +1,31 @@
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { UploadController } from './upload-controller'
-import { FileUpload, File, HttpRequest } from './upload-controller.protocols'
+import { FileUpload, File, HttpRequest, Validation } from './upload-controller.protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    file: {
+      name: '857716.jpg',
+      data: Buffer.from('any_value'),
+      size: 581426,
+      encoding: '7bit',
+      tempFilePath: 'any_type',
+      truncated: false,
+      mimetype: 'image/jpeg',
+      md5: '44df16492d29fcc3ad9ba1a3833d3a3d'
+    }
+  }
+})
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
 
 const makeFileUpload = (): FileUpload => {
   class FileUploadStub implements FileUpload {
@@ -12,38 +38,41 @@ const makeFileUpload = (): FileUpload => {
 interface sutTypes {
   sut: UploadController
   fileUploadStub: FileUpload
+  validationStub: Validation
 }
 
 const makeSut = (): sutTypes => {
   const fileUploadStub = makeFileUpload()
-  const sut = new UploadController(fileUploadStub)
-  return { sut, fileUploadStub }
+  const validationStub = makeValidation()
+  const sut = new UploadController(fileUploadStub, validationStub)
+  return { sut, fileUploadStub, validationStub }
 }
 
 describe('Upload Controller', () => {
   test('should call cloudnaryAdapter correct values', async () => {
     const { sut, fileUploadStub } = makeSut()
     const uploadSpy = jest.spyOn(fileUploadStub, 'upload')
-    const httpRequest: HttpRequest = {
-      body: {
-        file: {
-          name: '857716.jpg',
-          data: Buffer.from('any_value'),
-          size: 581426,
-          encoding: '7bit',
-          tempFilePath: 'any_type',
-          truncated: false,
-          mimetype: 'image/jpeg',
-          md5: '44df16492d29fcc3ad9ba1a3833d3a3d'
-        }
-      }
-    }
-    await sut.handle(httpRequest)
+    await sut.handle(makeFakeRequest())
     expect(uploadSpy).toHaveBeenCalledWith({
       name: '857716.jpg',
       size: 581426,
       tempFilePath: 'any_type',
       mimetype: 'image/jpeg'
     })
+  })
+
+  test('should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
--- src/presentation/controllers/upload/upload-controller.ts
+++ src/presentation/controllers/upload/upload-controller.ts
@@ -1,13 +1,19 @@
-import { ok } from '@/presentation/helpers/http/http-helper'
-import { HttpRequest, HttpResponse } from '../login/login-controller.protocols'
-import { Controller, FileUpload } from './upload-controller.protocols'
+import { badRequest, ok } from '@/presentation/helpers/http/http-helper'
+import { Controller, FileUpload, HttpRequest, HttpResponse, Validation } from './upload-controller.protocols'
 
 export class UploadController implements Controller {
-  constructor (private readonly cloudnaryAdapter: FileUpload) {}
+  constructor (
+    private readonly fileUpload: FileUpload,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, tempFilePath, mimetype, size } = httpRequest.body.file
-    const url = await this.cloudnaryAdapter.upload({ name, tempFilePath, mimetype, size })
-    return ok({ url })
+    const error = this.validation.validate(httpRequest.body)
+    if (!error) {
+      const { name, tempFilePath, mimetype, size } = httpRequest.body.file
+      const url = await this.fileUpload.upload({ name, tempFilePath, mimetype, size })
+      return ok({ url })
+    }
+    return badRequest(error)
   }
 }
