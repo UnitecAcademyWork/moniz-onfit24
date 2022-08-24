f621a7855fb29153ae172ee0a9d9ee9f8e6e18b8
moniz Massango <moniz.massango@gmail.com>
2022-08-24 17:03:01 +0200

    ensure: AddAccountInfoController calls Validation with correct values

--- src/presentation/controllers/account-info/add-account-info-controller.ts
+++ src/presentation/controllers/account-info/add-account-info-controller.ts
@@ -1,11 +1,15 @@
 import { methodNotAllowed, ok, serverError } from '@/presentation/helpers/http/http-helper'
-import { AddAccountInfo, Controller, HttpRequest, HttpResponse } from './add-account-info.protocols'
+import { AddAccountInfo, Controller, HttpRequest, HttpResponse, Validation } from './add-account-info.protocols'
 
 export class AddAccountInfoController implements Controller {
-  constructor (private readonly addAccountInfo: AddAccountInfo) {}
+  constructor (
+    private readonly addAccountInfo: AddAccountInfo,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { accountId, birth, gender, height, objective, weight } = httpRequest.body
       const accountInfo = await this.addAccountInfo.add({ accountId, birth, gender, height, objective, weight })
       if (!accountInfo) {
--- src/presentation/controllers/account-info/add-account-info.protocols.ts
+++ src/presentation/controllers/account-info/add-account-info.protocols.ts
@@ -1,3 +1,5 @@
 export * from '../../protocols'
 export * from '@/domain/models/account'
 export * from '@/domain/usecases/add-account'
+export * from '@/presentation/helpers/validators'
+export * from '@/presentation/protocols/validation'
