24a25c31f58ed7530bae19318f3284215adaf768
moniz Massango <moniz.massango@gmail.com>
2022-08-24 13:13:17 +0200

    refactor: move auth middlwares to separate files and optmaize routerAdapter

--- src/main/routes/objective-routes.ts
+++ src/main/routes/objective-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { routeAdapter } from '../adapters/express-route-adapter'
 import { makeAddObjectiveController } from '../factories/controllers/objective/add-objective/objective-controller-factory'
 import { makeLoadObjectiveController } from '../factories/controllers/objective/load-objective/load-objective-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware.-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware())
   router.post('/objective', adminAuth, routeAdapter(makeAddObjectiveController()))
   router.get('/objective', auth, routeAdapter(makeLoadObjectiveController()))
 }
--- src/main/routes/upload-routes.ts
+++ src/main/routes/upload-routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 import { makeUploadController } from '@/main/factories/controllers/upload/upload-controller-factory'
-import { routeUploadAdapter } from '@/main/adapters/express-upload-route-adapter'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware.-factory'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { routeAdapter } from '../adapters/express-route-adapter'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const auth = adaptMiddleware(makeAuthMiddleware())
-  router.post('/upload', auth, routeUploadAdapter(makeUploadController()))
+  router.post('/upload', auth, routeAdapter(makeUploadController()))
 }
