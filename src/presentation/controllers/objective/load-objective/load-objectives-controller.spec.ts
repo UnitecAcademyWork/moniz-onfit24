582526c9779c4810ba7978218385c2d435c95f24
moniz Massango <moniz.massango@gmail.com>
2022-08-24 11:23:48 +0200

    test: ensure LoadObjectiveController calls LoadObjective

---
+++ src/presentation/controllers/objective/load-objective/load-objectives-controller.spec.ts
@@ -0,0 +1,46 @@
+import { LoadObjectivesController } from './load-objectives-controller'
+import { LoadObjectives, ObjectiveModel } from './load-objectives-controller.protocol'
+
+const makeFakeObjective = (): ObjectiveModel[] => {
+  return [{
+    id: 'any_id',
+    name: 'any_name',
+    description: 'any_description',
+    icon: 'any_icon'
+  },
+  {
+    id: 'other_id',
+    name: 'other_name',
+    description: 'other_description',
+    icon: 'other_icon'
+  }]
+}
+
+const makeLoadObjectives = (): LoadObjectives => {
+  class LoadObjectivesStub implements LoadObjectives {
+    async load (): Promise<ObjectiveModel[]> {
+      return Promise.resolve(makeFakeObjective())
+    }
+  }
+  return new LoadObjectivesStub()
+}
+
+interface sutTypes {
+  loadObjectivesStub: LoadObjectives
+  sut: LoadObjectivesController
+}
+
+const makeSut = (): sutTypes => {
+  const loadObjectivesStub = makeLoadObjectives()
+  const sut = new LoadObjectivesController(loadObjectivesStub)
+  return { sut, loadObjectivesStub }
+}
+
+describe('LoadObjective Controller', () => {
+  test('should call LoadObjectives', async () => {
+    const { sut, loadObjectivesStub } = makeSut()
+    const loadSpy = jest.spyOn(loadObjectivesStub, 'load')
+    await sut.handle({})
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})
