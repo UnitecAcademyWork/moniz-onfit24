510686f9ee27fe7141709d1af02a8247bab11d61
moniz Massango <moniz.massango@gmail.com>
2022-08-24 11:37:22 +0200

    feat: ensure LoadObjectivesController returns 500 if loadObjectives throws

--- src/presentation/controllers/objective/load-objective/load-objectives-controller.ts
+++ src/presentation/controllers/objective/load-objective/load-objectives-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '@/presentation/helpers/http/http-helper'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../add-objective/add-objective-controller.protocols'
 import { Controller, LoadObjectives } from './load-objectives-controller.protocol'
 
@@ -6,7 +6,11 @@ export class LoadObjectivesController implements Controller {
   constructor (private readonly loadObjectives: LoadObjectives) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const objectives = await this.loadObjectives.load()
-    return ok(objectives)
+    try {
+      const objectives = await this.loadObjectives.load()
+      return ok(objectives)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
