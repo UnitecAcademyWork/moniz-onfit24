335767db3e03c9d382b04347084077c75e239ac5
moniz Massango <moniz.massango@gmail.com>
2022-08-29 10:28:59 +0200

    test: ensure AddProgramController calls AddProgram with correct values

--- src/presentation/controllers/program/add-program-controller.spec.ts
+++ src/presentation/controllers/program/add-program-controller.spec.ts
@@ -1,7 +1,9 @@
+import { ProgramModel } from '@/domain/models/program'
+import { AddProgramModel } from '@/domain/usecases/add-programa'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { AddProgramController } from './add-program-controller'
-import { HttpRequest, Validation } from './add-program-controller.protocols'
+import { AddProgram, HttpRequest, Validation } from './add-program-controller.protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -13,6 +15,15 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
+const makeFakeProgram = (): ProgramModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  description: 'any_description',
+  difficulty: 'any_difficulty',
+  duration: 'any_duration',
+  objective: ['any_objective', 'other_objective']
+})
+
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
@@ -22,15 +33,27 @@ const makeValidation = (): Validation => {
   return new ValidationStub()
 }
 
+const makeAddProgram = (): AddProgram => {
+  class AddProgramStub implements AddProgram {
+    async add (programa: AddProgramModel): Promise<ProgramModel> {
+      return Promise.resolve(makeFakeProgram())
+    }
+  }
+
+  return new AddProgramStub()
+}
+
 interface SutTypes {
+  addProgramStub: AddProgram
   validationStub: Validation
   sut: AddProgramController
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidation()
-  const sut = new AddProgramController(validationStub)
-  return { sut, validationStub }
+  const addProgramStub = makeAddProgram()
+  const sut = new AddProgramController(validationStub, addProgramStub)
+  return { sut, validationStub, addProgramStub }
 }
 
 describe('Add Program Controller', () => {
@@ -47,5 +70,18 @@ describe('Add Program Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('should call AddProgram with correct values', async () => {
+    const { sut, addProgramStub } = makeSut()
+    const addSpy = jest.spyOn(addProgramStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      description: 'any_description',
+      difficulty: 'any_difficulty',
+      duration: 'any_duration',
+      objective: ['any_objective', 'other_objective']
+    })
   })
 })
