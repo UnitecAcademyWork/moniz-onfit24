8549109b864090df3c15b7132c1111f08807d7ec
moniz Massango <moniz.massango@gmail.com>
2022-08-29 16:52:00 +0200

    test: ensure LoadProgramController returns 403 if invalid proramId is provided

--- src/presentation/controllers/program/load-programa/load-program-controller.spec.spec.ts
+++ src/presentation/controllers/program/load-programa/load-program-controller.spec.spec.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { LoadProgramController } from './load-program-controller'
 import { HttpRequest, LoadProgramById, ProgramModel } from './load-program-controller.protocols'
 
@@ -42,5 +44,12 @@ describe('LoadProgram Controller', () => {
     const loadByIdSpy = jest.spyOn(loadProgramByIdStub, 'loadById')
     await sut.handle(makeFakeRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('should return 403 if LoadProgramById returns null', async () => {
+    const { sut, loadProgramByIdStub } = makeSut()
+    jest.spyOn(loadProgramByIdStub, 'loadById').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('programId')))
   })
 })
