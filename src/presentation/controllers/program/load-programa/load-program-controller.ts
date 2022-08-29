5cc0638cf9b21c3f88cb501e2d2e5952de1adf39
moniz Massango <moniz.massango@gmail.com>
2022-08-29 16:56:02 +0200

    feat: ensure LoadProgramController returns 500 if loadProgramById throws

--- src/presentation/controllers/program/load-programa/load-program-controller.ts
+++ src/presentation/controllers/program/load-programa/load-program-controller.ts
@@ -1,15 +1,19 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadProgramById } from './load-program-controller.protocols'
 
 export class LoadProgramController implements Controller {
   constructor (private readonly loadProgramById: LoadProgramById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const program = await this.loadProgramById.loadById(httpRequest.params.programId)
-    if (!program) {
-      return forbidden(new InvalidParamError('programId'))
+    try {
+      const program = await this.loadProgramById.loadById(httpRequest.params.programId)
+      if (!program) {
+        return forbidden(new InvalidParamError('programId'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
