965556dd6b10ae251146d0199632036eb8335580
moniz Massango <moniz.massango@gmail.com>
2022-08-29 16:51:24 +0200

    feat: ensure LoadProgramController returns 403 if invalid proramId is provided

--- src/presentation/controllers/program/load-programa/load-program-controller.ts
+++ src/presentation/controllers/program/load-programa/load-program-controller.ts
@@ -1,10 +1,15 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadProgramById } from './load-program-controller.protocols'
 
 export class LoadProgramController implements Controller {
   constructor (private readonly loadProgramById: LoadProgramById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadProgramById.loadById(httpRequest.params.programId)
+    const program = await this.loadProgramById.loadById(httpRequest.params.programId)
+    if (!program) {
+      return forbidden(new InvalidParamError('programId'))
+    }
     return null
   }
 }
