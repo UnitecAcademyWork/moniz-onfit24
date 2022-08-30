8605e7192907de3e785284525e57cbc4d347415a
moniz Massango <moniz.massango@gmail.com>
2022-08-30 15:22:40 +0200

    feat: ensure DbDeleteProgram returns the deleted program

--- src/data/usecases/program/delete-program/db-delete-program.ts
+++ src/data/usecases/program/delete-program/db-delete-program.ts
@@ -1,10 +1,10 @@
-import { DeleteProgram, DeleteProgramRepository } from './db-delete-program.protocols'
+import { DeleteProgram, DeleteProgramRepository, ProgramModel } from './db-delete-program.protocols'
 
 export class DbDeleteProgram implements DeleteProgram {
   constructor (private readonly deleteProgramRepository: DeleteProgramRepository) {}
 
-  async delete (programId: string): Promise<void> {
-    await this.deleteProgramRepository.delete(programId)
-    return null
+  async delete (programId: string): Promise<ProgramModel> {
+    const program = await this.deleteProgramRepository.delete(programId)
+    return program
   }
 }
--- src/domain/usecases/program/delete-program.ts
+++ src/domain/usecases/program/delete-program.ts
@@ -1,3 +1,5 @@
+import { ProgramModel } from '@/domain/models/program'
+
 export interface DeleteProgram {
-  delete: (programId: string) => Promise<void>
+  delete: (programId: string) => Promise<ProgramModel>
 }
