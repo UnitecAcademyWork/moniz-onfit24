f1e5c75c09580700662cf058154e5781ae5e5278
moniz Massango <moniz.massango@gmail.com>
2022-08-30 15:49:21 +0200

    test: ensure DbDeleteProgram returns 204 on delete success

--- src/presentation/controllers/program/delete-program/delete-program-controller.spec.ts
+++ src/presentation/controllers/program/delete-program/delete-program-controller.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, noContent, serverError } from '@/presentation/helpers/http/http-helper'
 import { DeleteProgramController } from './delete-program-controller'
 import { DeleteProgram, HttpRequest, ProgramModel } from './delete-program-controller.protocols'
 
@@ -59,5 +59,11 @@ describe('DeleteProgram Controller', () => {
     jest.spyOn(deleteProgramStub, 'delete').mockRejectedValueOnce(new Error())
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
