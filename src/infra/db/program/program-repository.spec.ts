7355d5ba071edca426972b8a3fa194177926c14a
moniz Massango <moniz.massango@gmail.com>
2022-08-30 10:08:30 +0200

    test: ensure ProgramRepository returns a program on loadById success

--- src/infra/db/program/program-repository.spec.ts
+++ src/infra/db/program/program-repository.spec.ts
@@ -37,4 +37,49 @@ describe('Program Repository', () => {
       expect(program.objective).toEqual(['any_objective', 'other_objective'])
     })
   })
+
+  describe('add()', () => {
+    test('should return a program on add success', async () => {
+      const sut = makeSut()
+      const program = await sut.add({
+        name: 'any_name',
+        description: 'any_description',
+        difficulty: 'any_difficulty',
+        duration: 'any_duration',
+        equipment: ['any_equipment', 'other_equipment'],
+        objective: ['any_objective', 'other_objective']
+      })
+      expect(program).toBeTruthy()
+      expect(program.id).toBeTruthy()
+      expect(program.name).toBe('any_name')
+      expect(program.description).toBe('any_description')
+      expect(program.difficulty).toBe('any_difficulty')
+      expect(program.duration).toBe('any_duration')
+      expect(program.equipment).toEqual(['any_equipment', 'other_equipment'])
+      expect(program.objective).toEqual(['any_objective', 'other_objective'])
+    })
+  })
+
+  describe('loadById()', () => {
+    test('should return a program on loadById success', async () => {
+      const sut = makeSut()
+      const program = await sut.add({
+        name: 'any_name',
+        description: 'any_description',
+        difficulty: 'any_difficulty',
+        duration: 'any_duration',
+        equipment: ['any_equipment', 'other_equipment'],
+        objective: ['any_objective', 'other_objective']
+      })
+      const loadedProgram = await sut.loadById(program.id)
+      expect(loadedProgram).toBeTruthy()
+      expect(loadedProgram.id).toBeTruthy()
+      expect(loadedProgram.name).toBe('any_name')
+      expect(loadedProgram.description).toBe('any_description')
+      expect(loadedProgram.difficulty).toBe('any_difficulty')
+      expect(loadedProgram.duration).toBe('any_duration')
+      expect(loadedProgram.equipment).toEqual(['any_equipment', 'other_equipment'])
+      expect(loadedProgram.objective).toEqual(['any_objective', 'other_objective'])
+    })
+  })
 })
