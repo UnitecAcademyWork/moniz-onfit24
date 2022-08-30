8bbb768d80e1a87aeca4269695857aaf63dfbb3b
moniz Massango <moniz.massango@gmail.com>
2022-08-30 15:42:15 +0200

    feat: ensure DbDeleteProgram returns 403 if DeleteProgram returns null

--- src/presentation/controllers/program/delete-program/delete-program-controller.ts
+++ src/presentation/controllers/program/delete-program/delete-program-controller.ts
@@ -1,10 +1,14 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, DeleteProgram, HttpRequest, HttpResponse } from './delete-program-controller.protocols'
 
 export class DeleteProgramController implements Controller {
   constructor (private readonly deleteProgram: DeleteProgram) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.deleteProgram.delete(httpRequest.params.programId)
-    return null
+    const program = await this.deleteProgram.delete(httpRequest.params.programId)
+    if (!program) {
+      return forbidden(new InvalidParamError('programId'))
+    }
   }
 }
