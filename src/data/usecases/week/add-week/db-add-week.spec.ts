5170f56b436483dc1c10745c055e74c6d2876799
moniz Massango <moniz.massango@gmail.com>
2022-08-31 10:21:30 +0200

    test: ensure DbAddWeek calls AddWeekRepository with correct values

---
+++ src/data/usecases/week/add-week/db-add-week.spec.ts
@@ -0,0 +1,65 @@
+import { DbAddWeek } from './db-add-week'
+import { AddWeekModel, AddWeekRepository, WeekModel } from './db-add-week.protocols'
+
+const makeFakeWeek = (): WeekModel => ({
+  id: 'any_id',
+  programId: 'program_id',
+  goals: ['any_goal', 'other_goal'],
+  exercises: [{
+    duration: 'any_duration',
+    title: 'any_title',
+    uri: 'any_uri',
+    url: 'any_url'
+  }]
+})
+
+const makeFakeWeekData = (): AddWeekModel => ({
+  programId: 'program_id',
+  goals: ['any_goal', 'other_goal'],
+  exercises: [{
+    duration: 'any_duration',
+    title: 'any_title',
+    uri: 'any_uri',
+    url: 'any_url'
+  }]
+})
+
+const makeAddWeekRepository = (): AddWeekRepository => {
+  class AddWeekRepositoryStub implements AddWeekRepository {
+    async add (week: AddWeekModel): Promise<WeekModel> {
+      return Promise.resolve(makeFakeWeek())
+    }
+  }
+
+  return new AddWeekRepositoryStub()
+}
+
+interface SutTypes {
+  addWeekRepositoryStub: AddWeekRepository
+  sut: DbAddWeek
+}
+
+const makeSut = (): SutTypes => {
+  const addWeekRepositoryStub = makeAddWeekRepository()
+  const sut = new DbAddWeek(addWeekRepositoryStub)
+
+  return { sut, addWeekRepositoryStub }
+}
+
+describe('DbAddWeek', () => {
+  test('should call addWeekRepository with correct values', async () => {
+    const { sut, addWeekRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addWeekRepositoryStub, 'add')
+    await sut.add(makeFakeWeekData(), 'any_id')
+    expect(addSpy).toHaveBeenCalledWith({
+      programId: 'program_id',
+      goals: ['any_goal', 'other_goal'],
+      exercises: [{
+        duration: 'any_duration',
+        title: 'any_title',
+        uri: 'any_uri',
+        url: 'any_url'
+      }]
+    }, 'any_id')
+  })
+})
