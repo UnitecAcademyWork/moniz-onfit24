22f8284e525266fde5ddaffedc9dddd5d77928f4
moniz Massango <moniz.massango@gmail.com>
2022-08-31 16:53:50 +0200

    feat: ensure LoadWeekController return 403 if LoadWeekById returns null

--- src/presentation/controllers/week/load-week/load-week-controller.ts
+++ src/presentation/controllers/week/load-week/load-week-controller.ts
@@ -1,10 +1,15 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadWeekById } from './load-week-controller.protocols'
 
 export class LoadWeekController implements Controller {
   constructor (private readonly loadWeekById: LoadWeekById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadWeekById.loadById(httpRequest.params.weekId)
-    return Promise.resolve(null)
+    const week = await this.loadWeekById.loadById(httpRequest.params.weekId)
+    if (!week) {
+      return forbidden(new InvalidParamError('weekId'))
+    }
+    return null
   }
 }
