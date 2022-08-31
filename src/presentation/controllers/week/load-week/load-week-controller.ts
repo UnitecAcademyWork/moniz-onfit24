edee5b6be3e067f52bac1ef44459b2a8ed1f20c5
moniz Massango <moniz.massango@gmail.com>
2022-08-31 16:58:12 +0200

    feat: ensure LoadWeekController returns 500 if LoadWeekById throws

--- src/presentation/controllers/week/load-week/load-week-controller.ts
+++ src/presentation/controllers/week/load-week/load-week-controller.ts
@@ -1,15 +1,19 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadWeekById } from './load-week-controller.protocols'
 
 export class LoadWeekController implements Controller {
   constructor (private readonly loadWeekById: LoadWeekById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const week = await this.loadWeekById.loadById(httpRequest.params.weekId)
-    if (!week) {
-      return forbidden(new InvalidParamError('weekId'))
+    try {
+      const week = await this.loadWeekById.loadById(httpRequest.params.weekId)
+      if (!week) {
+        return forbidden(new InvalidParamError('weekId'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
