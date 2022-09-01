f3ce4d99c8a974f9d84ef8f640f862a452d5b807
moniz Massango <moniz.massango@gmail.com>
2022-09-01 15:38:14 +0200

    feat: ensure LoadWeeksController returns 500 if LoadWeeks thorws

--- src/presentation/controllers/week/load-weeks/load-weeks-controller.ts
+++ src/presentation/controllers/week/load-weeks/load-weeks-controller.ts
@@ -1,11 +1,15 @@
-import { noContent, ok } from '@/presentation/helpers/http/http-helper'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadWeeks } from './load-weeks-controller.protocols'
 
 export class LoadWeeksController implements Controller {
   constructor (private readonly loadWeeks: LoadWeeks) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const weeks = await this.loadWeeks.loadAll()
-    return weeks.length ? ok(weeks) : noContent()
+    try {
+      const weeks = await this.loadWeeks.loadAll()
+      return weeks.length ? ok(weeks) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
