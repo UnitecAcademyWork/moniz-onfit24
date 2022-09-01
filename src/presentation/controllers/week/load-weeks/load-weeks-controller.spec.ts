8d357e24908d31e97cca63351810631ba46be2b6
moniz Massango <moniz.massango@gmail.com>
2022-09-01 15:30:04 +0200

    test: ensure LoadWeeksController calls LoadWeeks

---
+++ src/presentation/controllers/week/load-weeks/load-weeks-controller.spec.ts
@@ -0,0 +1,52 @@
+import { LoadWeeksController } from './load-weeks-controller'
+import { LoadWeeks, WeekModel } from './load-weeks-controller.protocols'
+
+const makeFakeWeeks = (): WeekModel[] => ([{
+  id: 'any_id',
+  goals: ['any_goal', 'other_goal'],
+  exercises: [{
+    duration: 'any_duration',
+    title: 'any_title',
+    uri: 'any_uri',
+    url: 'any_url'
+  }]
+},
+{
+  id: 'other_id',
+  goals: ['other_goal', 'other_goal'],
+  exercises: [{
+    duration: 'other_duration',
+    title: 'other_title',
+    uri: 'other_uri',
+    url: 'other_url'
+  }]
+}])
+
+const makeLoadWeeks = (): LoadWeeks => {
+  class LoadWeeksStub implements LoadWeeks {
+    async loadAll (): Promise<WeekModel[]> {
+      return Promise.resolve(makeFakeWeeks())
+    }
+  }
+  return new LoadWeeksStub()
+}
+
+interface SutTypes {
+  loadWeeksStub: LoadWeeks
+  sut: LoadWeeksController
+}
+
+const makeSut = (): SutTypes => {
+  const loadWeeksStub = makeLoadWeeks()
+  const sut = new LoadWeeksController(loadWeeksStub)
+  return { sut, loadWeeksStub }
+}
+
+describe('LoadWeeks Controller', () => {
+  test('should call LoadWeeks', async () => {
+    const { sut, loadWeeksStub } = makeSut()
+    const loadAllSpy = jest.spyOn(loadWeeksStub, 'loadAll')
+    await sut.handle({})
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
