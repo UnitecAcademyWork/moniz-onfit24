52bb9da3cf26830ddc3897c063f54207e66189ed
moniz Massango <moniz.massango@gmail.com>
2022-09-01 15:32:56 +0200

    feat: ensure LoadWeeksController returns 200 on success

--- src/presentation/controllers/week/load-weeks/load-weeks-controller.ts
+++ src/presentation/controllers/week/load-weeks/load-weeks-controller.ts
@@ -1,10 +1,11 @@
+import { ok } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadWeeks } from './load-weeks-controller.protocols'
 
 export class LoadWeeksController implements Controller {
   constructor (private readonly loadWeeks: LoadWeeks) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadWeeks.loadAll()
-    return null
+    const weeks = await this.loadWeeks.loadAll()
+    return ok(weeks)
   }
 }
