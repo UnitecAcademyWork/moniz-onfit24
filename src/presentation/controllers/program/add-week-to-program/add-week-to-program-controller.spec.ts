17cd4b21bd64ff2ef746c68c1a2ae8896c6748a1
moniz Massango <moniz.massango@gmail.com>
2022-09-02 14:19:42 +0200

    test: ensure AddWeekToProgramController calls AddWeekToProgram with correct values

---
+++ src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.spec.ts
@@ -0,0 +1,61 @@
+import { AddWeekToProgramController } from './add-week-to-program-controller'
+import { AddWeekToProgram, HttpRequest, ProgramModel } from './add-week-to-program-controller.protocols'
+
+const makeFakeProgram = (): ProgramModel => ({
+  id: 'any_id',
+  url: 'any_url',
+  name: 'any_name',
+  description: 'any_description',
+  difficulty: 'any_difficulty',
+  duration: 'any_duration',
+  objective: ['any_objective', 'other_objective'],
+  equipment: ['any_equipment', 'other_equipment'],
+  weeks: [
+    {
+      id: 'exercise_id',
+      goals: ['any_goal'],
+      exercises: [{
+        duration: 'any_duration',
+        title: 'any_title',
+        uri: 'any_uri',
+        url: 'any_url'
+      }]
+    }
+  ]
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    programId: 'program_id',
+    weekId: 'week_id'
+  }
+})
+
+const makeAddWeekToProgram = (): AddWeekToProgram => {
+  class AddWeekToProgramStub implements AddWeekToProgram {
+    async associate (programId: string, weekId: string): Promise<ProgramModel> {
+      return Promise.resolve(makeFakeProgram())
+    }
+  }
+  return new AddWeekToProgramStub()
+}
+
+interface SutTypes {
+  addWeekToProgramStub: AddWeekToProgram
+  sut: AddWeekToProgramController
+}
+
+const makeSut = (): SutTypes => {
+  const addWeekToProgramStub = makeAddWeekToProgram()
+  const sut = new AddWeekToProgramController(addWeekToProgramStub)
+  return { sut, addWeekToProgramStub }
+}
+
+describe('AddWeekToProgram Controller', () => {
+  test('should call AddWeekToProgram with correct values', async () => {
+    const { sut, addWeekToProgramStub } = makeSut()
+    const associateSpy = jest.spyOn(addWeekToProgramStub, 'associate')
+    await sut.handle(makeFakeRequest())
+    expect(associateSpy).toHaveBeenCalledWith('program_id', 'week_id')
+  })
+})
