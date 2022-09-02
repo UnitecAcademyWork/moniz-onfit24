f860fd7aa9bb3819a548202fd8ecfc10c93924e2
moniz Massango <moniz.massango@gmail.com>
2022-09-02 14:24:02 +0200

    feat: ensure AddWeekToProgramController returns 403 if AddWeekToProgram returns null

--- src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
+++ src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { AddWeekToProgram, Controller, HttpRequest, HttpResponse } from './add-week-to-program-controller.protocols'
 
 export class AddWeekToProgramController implements Controller {
@@ -5,7 +7,9 @@ export class AddWeekToProgramController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { programId, weekId } = httpRequest.body
-    await this.addWeekToProgram.associate(programId, weekId)
-    return null
+    const program = await this.addWeekToProgram.associate(programId, weekId)
+    if (!program) {
+      return forbidden(new InvalidParamError('invalidId'))
+    }
   }
 }
