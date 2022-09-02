35f3d9b56cb2cae31b882d13625f6b44fd48dcff
moniz Massango <moniz.massango@gmail.com>
2022-09-02 14:47:25 +0200

    feat: ensure AddWeekToProgramController calls Validation and returns error if validation fails

--- src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.protocols.ts
+++ src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.protocols.ts
@@ -1,3 +1,5 @@
 export * from '@/presentation/protocols'
 export * from '@/domain/models/program'
 export * from '@/domain/usecases/program/add-week-to-program'
+export * from '@/presentation/helpers/validators'
+export * from '@/presentation/protocols/validation'
--- src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
+++ src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
@@ -1,12 +1,19 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
-import { AddWeekToProgram, Controller, HttpRequest, HttpResponse } from './add-week-to-program-controller.protocols'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { AddWeekToProgram, Controller, HttpRequest, HttpResponse, Validation } from './add-week-to-program-controller.protocols'
 
 export class AddWeekToProgramController implements Controller {
-  constructor (private readonly addWeekToProgram: AddWeekToProgram) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addWeekToProgram: AddWeekToProgram
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { programId, weekId } = httpRequest.body
       const program = await this.addWeekToProgram.associate(programId, weekId)
       if (!program) {
