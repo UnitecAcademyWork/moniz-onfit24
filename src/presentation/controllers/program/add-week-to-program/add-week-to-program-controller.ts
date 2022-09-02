fd51aa8426bca4589fd2b3a6d268688e1f0f4e3d
moniz Massango <moniz.massango@gmail.com>
2022-09-02 14:38:59 +0200

    feat: ensure AddWeekToProgramController returns 500 if AddWeekToProgram throws

--- src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
+++ src/presentation/controllers/program/add-week-to-program/add-week-to-program-controller.ts
@@ -1,15 +1,19 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { AddWeekToProgram, Controller, HttpRequest, HttpResponse } from './add-week-to-program-controller.protocols'
 
 export class AddWeekToProgramController implements Controller {
   constructor (private readonly addWeekToProgram: AddWeekToProgram) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { programId, weekId } = httpRequest.body
-    const program = await this.addWeekToProgram.associate(programId, weekId)
-    if (!program) {
-      return forbidden(new InvalidParamError('invalidId'))
+    try {
+      const { programId, weekId } = httpRequest.body
+      const program = await this.addWeekToProgram.associate(programId, weekId)
+      if (!program) {
+        return forbidden(new InvalidParamError('invalidId'))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
