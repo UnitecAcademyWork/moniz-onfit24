28f9919a257ac18a86424a3fba6704239e653bcb
moniz Massango <moniz.massango@gmail.com>
2022-09-05 12:54:45 +0200

    test: ensure AddRecipeController calls AddRecipe with correct values

--- src/presentation/controllers/recipe/add-recipe/add-recipe-controller.spec.ts
+++ src/presentation/controllers/recipe/add-recipe/add-recipe-controller.spec.ts
@@ -112,4 +112,39 @@ describe('AddRecipe Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
+
+  test('should call AddRecipe with correct values', async () => {
+    const { sut, addRecipeStub } = makeSut()
+    const addSpy = jest.spyOn(addRecipeStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      description: 'any_description',
+      prepTime: 'any_preTime',
+      cookTime: 'any_cookTime',
+      difficulty: 'any_difficulty',
+      serves: 'any_serving',
+      url: 'any_url',
+      nutrition: {
+        kcal: 'any_kcal',
+        fat: 'any_fat',
+        saturates: 'any_saturates',
+        carbs: 'any_carbs',
+        sugar: 'any_sugar',
+        fibre: 'any_fibre',
+        protein: 'any_protein',
+        salt: 'any_salt'
+      },
+      tags: ['any_tag', 'other_tag'],
+      ingredients: [{
+        name: 'any_name',
+        quantity: 'any_quantity'
+      },
+      {
+        name: 'other_name',
+        quantity: 'other_quantity'
+      }],
+      steps: ['any_step', 'other_step']
+    })
+  })
 })
