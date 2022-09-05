2831cb98f6cf673cfea9a6bef778a4d0906d2d80
moniz Massango <moniz.massango@gmail.com>
2022-09-05 12:56:22 +0200

    feat: ensure AddRecipeController returns 500 if AddRecipe throws

--- src/presentation/controllers/recipe/add-recipe/add-recipe-controller.ts
+++ src/presentation/controllers/recipe/add-recipe/add-recipe-controller.ts
@@ -1,16 +1,20 @@
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import { AddRecipe, Controller, HttpRequest, HttpResponse, Validation } from './add-recipe-controller.protocols'
 
 export class AddRecipeController implements Controller {
   constructor (private readonly validation: Validation, private readonly addRecipe: AddRecipe) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { name, description, prepTime, cookTime, difficulty, serves, url, nutrition, tags, ingredients, steps } = httpRequest.body
+      await this.addRecipe.add({ name, description, prepTime, cookTime, difficulty, serves, url, nutrition, tags, ingredients, steps })
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    const { name, description, prepTime, cookTime, difficulty, serves, url, nutrition, tags, ingredients, steps } = httpRequest.body
-    await this.addRecipe.add({ name, description, prepTime, cookTime, difficulty, serves, url, nutrition, tags, ingredients, steps })
-    return null
   }
 }
